6c2a6548002e3dd40f5365163d5fe64f7d0c9b84
Iuri <80118088+IuryDevLab@users.noreply.github.com>
2023-02-08 18:24:40 -0300

    Create front with vue

---
+++ Frontend/client/src/views/Details.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container mt-5">
+    <h1>Name: {{ products.name }}</h1>
+    <h3>Quantidaty: {{ products.quantity }}</h3>
+    <h3>Price: $ {{ products.price }}</h3>
+    <h3>Description: {{ products.description }}</h3>
+     
+    <router-link :to="{name:'EditProduct', params:{id:id}}">
+    <button class="btn btn-success mx-3 mt-3">Update</button>
+    </router-link>
+  
+    <button class="btn btn-danger mt-3" @click="deleteProduct">Delete</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Detail',
+  data(){
+    return{
+      products: []
+    }
+  },
+  props:{
+    id:{
+      type:[Number, String],
+      required:true
+    }
+  },
+  methods:{
+    getProducts(){
+      axios
+        .get(`/product/${this.id}/`)
+        .then(response => {
+          this.products = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    deleteProduct() {
+    axios
+        .delete(`/product/${this.id}/`)
+        .then(res=>{
+          this.$router.push({
+            name:'Home'
+          })
+        })
+  }
+  },
+  created() {
+    this.getProducts()
+  }
+}
+</script>
+
+<style>
+
+</style>
--- Frontend/client/src/views/Home.vue
+++ Frontend/client/src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col md-12">
+        <div class="jumbotron p-3">
+            <br><br><br>  
+            <h2><b>Product Management</b></h2>
+            <br>
+            <table class="table table-dark">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Name </th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="product in products" 
+                  :key="product.id"
+                >
+                  <td>{{ product.id }}</td>
+                  <td>{{ product.name }}</td>
+                  <td>{{ product.quantity }}</td>
+                  <td>$ {{ product.price }}</td>
+                  <td>
+                    <router-link :to="{name:'Details', params:{id:product.id}}">
+                      <button type="button" class="btn btn-success">Details</button>
+                    </router-link></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+        return{
+            products: []
+        }
+    },
+    mounted(){
+        this.getProducts()
+    },
+    methods:{
+        getProducts(){
+            axios
+                .get('/product/')
+                .then(response => {
+                    this.products = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+        })
+    }
   }
 }
 </script>
